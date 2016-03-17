7fbf4dacb2e4a04ba9fcec5f6eb224ff61678713
Ryoko Akizuki <ryokoakizuki@gmail.com>
2016-03-17 12:03:47 +0800

    fixed errors in variable declarations; added package php5-curl; clone letsencrypt repo in working dir.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SERVER_ADMIN = "user@example.com"
-SERVER_NAME = "www.example.com"
-SERVER_ALIAS = "example.com"
+SERVER_ADMIN="user@example.com"
+SERVER_NAME="www.example.com"
+SERVER_ALIAS="example.com"
 
 sed -i "s/SERVER_ADMIN_PLACEHOLDER/$SERVER_ADMIN/g" target-sslsite.conf
 sed -i "s/SERVER_NAME_PLACEHOLDER/$SERVER_NAME/g" target-sslsite.conf
@@ -10,7 +10,7 @@ sed -i "s/SERVER_ALIAS_PLACEHOLDER/$SERVER_ALIAS/g" target-sslsite.conf
 
 # install LAMP stack
 apt-get update
-apt-get -y install apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt
+apt-get -y install apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt php5-curl
 
 mysql_install_db
 # secure mysql
@@ -31,7 +31,6 @@ cp ./target-apache2.conf /etc/apache2/apache2.conf
 cp ./target-sslsite.conf /etc/apache2/sites-enabled/sslsite.conf
 
 apt-get -y install git
-cd ~
 git clone https://github.com/letsencrypt/letsencrypt.git
 cd letsencrypt
 ./letsencrypt-auto
